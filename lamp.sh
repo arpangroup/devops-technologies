377b9842f248a0bc558141f546a0385ac35f8415
arpan jana <arpangroup1@gmail.com>
2022-06-26 17:58:04 +0530

    fix

--- lamp.sh
+++ lamp.sh
@@ -15,4 +15,6 @@ echo "${BOLDGREEN}\n\n\n [1/10] Updating Apt Packages and upgrading latest patch
 
 # Step-2: Installing Apache2
 echo "${BOLDGREEN}\n\n\n [2/10] Installing Apache2 & other packages.........${ENDCOLOR}"
+echo "passing DomainName as : "
+echo $DOMAIN_NAME
 sh ${SCRIPT_DIR}/install-apache.sh $DOMAIN_NAME
