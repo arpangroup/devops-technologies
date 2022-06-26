81dd9c35bf951667b3c04bb4cf7a01d18d000c13
arpan jana <arpangroup1@gmail.com>
2022-06-26 19:05:03 +0530

    fix

--- .scripts/ubuntu/install-apache.sh
+++ .scripts/ubuntu/install-apache.sh
@@ -10,11 +10,10 @@ GREEN="32"
 BOLDGREEN="\e[1;${GREEN}m"
 ITALICRED="\e[3;${RED}m"
 ENDCOLOR="\e[0m"
-DIVIDER="\n{$BOLDGREEN}++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 
 
 echo "$BOLDGREEN[Apache : 1/10]$ENDCOLOR Installing Apache2 & other packages............................"
-sudo apt -y install apache2 \
+sudo apt install -qq -y apache2 \
     software-properties-common \
     curl \
     zip \
@@ -30,36 +29,16 @@ sudo apt -y install apache2 \
 # sudo ufw app list
 
 # Only allow traffic on port 80, use the Apache profile....Adjusting the firewall settings to allow HTTP traffic
-printf "$BOLDGREEN[Apache : 2/10]$ENDCOLOR Adjusting the firewall settings to allow HTTP traffic.........\n"
+echo "$BOLDGREEN[Apache : 2/10]$ENDCOLOR Adjusting the firewall settings to allow HTTP traffic........."
 sudo ufw allow "Apache Full"
-
-
-echo "${BOLDGREEN}++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++${ENDCOLOR}"
-echo "${BOLDGREEN}++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++${ENDCOLOR}"
-echo "                Apache Server installed successfully "
-ip addr show eth0 | grep inet | awk '{ print $2; }' | sed 's/\/.*$//'
-echo "${BOLDGREEN}++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++${ENDCOLOR}"
-echo "${BOLDGREEN}++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++${ENDCOLOR}"
-
-# Prompt to continue
-echo "Do you want to configure apache with the doimain $BOLDGREEN $DOMAIN_NAME $ENDCOLOR";
-while true; do
-	read -p "Continue [Y/N]? " cnt1
-	case $cnt1 in
-		"") break;;
-		[Yy]* ) break;;
-		[Nn]* ) exit;;
-		* ) printf "Please answer Y or N\n";;
-	esac
-done
 
 # # Show current firewall status
 # sudo ufw status
 
-printf '\n[Apache : 3/10] Enabling the apache service\....n'
+echo '[Apache : 3/10] Enabling the apache service....'
 sudo systemctl enable apache2
 
-printf "\n\n$BOLDGREEN[Apache : 4/10]$ENDCOLOR Updating apache config.....\n"
+echo "$BOLDGREEN[Apache : 4/10]$ENDCOLOR Updating apache config....."
 cat << EOF >/etc/apache2/sites-available/$DOMAIN_NAME.conf
 <VirtualHost *:80>
     ServerName $DOMAIN_NAME
@@ -82,21 +61,41 @@ sudo systemctl reload apache2
 
 
 # Creating the web root path and set the permissions
-echo "$BOLDGREEN[Apache : 6/10] Creating the web root path and set the permissions...."
-sudo mkdir -p ${WEB_ROOT}/${DOMAIN_NAME}
+echo "$BOLDGREEN[Apache : 6/10]$ENDCOLOR Creating the web root path and set the permissions...."
+sudo mkdir -p $WEB_ROOT/$DOMAIN_NAME
 # Set Permissions  
-echo "$BOLDGREEN[Apache : 7/10] $ENDCOLOR Set Permissions to WebRoot($WEB_ROOT/$DOMAIN_NAME)....."
-sudo chown -R www-data:www-data ${WEB_ROOT}
-sudo chown -R $USER:$USER ${WEB_ROOT}/{$DOMAIN_NAME}
+echo "$BOLDGREEN[Apache : 7/10]$ENDCOLOR Set Permissions to WebRoot($WEB_ROOT/$DOMAIN_NAME)....."
+sudo chown -R www-data:www-data $WEB_ROOT
+sudo chown -R $USER:$USER $WEB_ROOT/$DOMAIN_NAME
 
 # Creating the index.html file
-echo "$BOLDGREEN [Apache : 8/10]$ENDCOLOR creating inex.html and  test.php file"
+echo "$BOLDGREEN[Apache : 8/10]$ENDCOLOR Generating inex.html, test.php file"
 echo "$INDEX_FILE_CONTENT" > $WEB_ROOT/$DOMAIN_NAME/index.html
 echo "<?php phpinfo(); ?>" > $WEB_ROOT/$DOMAIN_NAME/test.php
 
 echo "$BOLDGREEN[Apache : 9/10]$ENDCOLOR reload apache service"
 sudo service apache2 restart
 
-printf "\n$BOLDGREEN [Apache : 10/10]$ENDCOLOR status of apache service \n" 
+echo "$BOLDGREEN[Apache : 10/10]$ENDCOLOR status of apache service " 
 APACHE_VERSION=apache2 -v | grep -Po '(?<=Server version: Apache/)[^?]+'
 apache2 -v
+
+
+echo "${BOLDGREEN}++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++${ENDCOLOR}"
+echo "${BOLDGREEN}++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++${ENDCOLOR}"
+echo "                Apache Server installed successfully "
+ip addr show eth0 | grep inet | awk '{ print $2; }' | sed 's/\/.*$//'
+echo "${BOLDGREEN}++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++${ENDCOLOR}"
+echo "${BOLDGREEN}++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++${ENDCOLOR}"
+
+# Prompt to continue
+# echo "Do you want to configure apache with the doimain $BOLDGREEN $DOMAIN_NAME $ENDCOLOR";
+# while true; do
+# 	read -p "Continue [Y/N]? " cnt1
+# 	case $cnt1 in
+# 		"") break;;
+# 		[Yy]* ) break;;
+# 		[Nn]* ) exit;;
+# 		* ) printf "Please answer Y or N\n";;
+# 	esac
+# done
