1e77906711c4b9f6543ed0b650b733cd416457b0
arpan jana <arpangroup1@gmail.com>
2022-06-26 20:07:48 +0530

    fix

--- lamp.sh
+++ lamp.sh
@@ -5,7 +5,7 @@
 #######################################################  
 ROOT_DIR=./.scripts
 SCRIPT_DIR=${ROOT_DIR}/ubuntu
-. $ROOT_DIR/env.sh # [ Note: There is a space between the two dots(.) ]
+sh $ROOT_DIR/env.sh # [ Note: There is a space between the two dots(.) ]
 . ./config.sh # [ Note: There is a space between the two dots(.) ]
 
 
